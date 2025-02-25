167ed84748c33c09450ba6560a3a1f6c69bfa3e2
Eduardo-Decarli <decarliedu@gmail.com>
2025-02-25 19:38:05 -0300

    feat: finish project

--- new-game-teste/player.gd
+++ new-game-teste/player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
+const speed = 300.0
+const jump_speed = -500.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		$SoundJump.play()
+		velocity.y = jump_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction*speed
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		$AnimatedSprite2D.stop()
 	move_and_slide()
